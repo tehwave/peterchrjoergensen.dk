ef756f403e942946076b9906f499f22abd5d1ddd
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-04 19:42:50 +0200

    :boom: Initial commit

---
+++ index.php
@@ -0,0 +1,163 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Peter C. Jørgensen</title>
+
+		<!--
+			  ____      _               ____         _
+			 |  _ \ ___| |_ ___ _ __   / ___|       | | ____  _ __ __ _  ___ _ __  ___  ___ _ __
+			 | |_) / _ \ __/ _ \ '__| | |        _  | |/ _//\| '__/ _` |/ _ \ '_ \/ __|/ _ \ '_ \
+			 |  __/  __/ ||  __/ |    | |___ _  | |_| | (//) | | | (_| |  __/ | | \__ \  __/ | | |
+			 |_|   \___|\__\___|_|     \____(_)  \___/ \//__/|_|  \__, |\___|_| |_|___/\___|_| |_|
+			                                                      |___/
+										Designed and developed by
+							Peter C. Jørgensen | peterchrjoergensen.dk | @tehwave
+		 -->
+
+		<!-- SEO -->
+		<meta name="robots" content="index, follow">
+		<meta name="title" content="Peter C. Jørgensen">
+		<meta name="author" content="Peter C. Jørgensen">
+		<meta name="description" content="The personal website of Peter C. Jørgensen. It includes his portfolio, curriculum vitae, and blog.">
+
+		<!-- Favicon -->
+		<link rel="manifest" href="/manifest.json">
+		<meta name="theme-color" content="#0fa0ce">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+
+		<!-- Open Graph -->
+		<meta property="og:type" content="profile">
+		<meta property="profile:first_name" content="Peter">
+		<meta property="profile:last_name" content="Jørgensen">
+		<meta property="og:title" content="Peter C. Jørgensen">
+		<meta property="og:url" content="https://peterchrjoergensen.dk">
+		<meta property="og:image" content="https://peterchrjoergensen.dk/img/peter.jpg">
+		<meta property="og:description" content="The personal website of Peter C. Jørgensen. It includes his portfolio, curriculum vitae, and blog.">
+
+		<!-- Twitter -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@tehwave">
+		<meta name="twitter:title" content="Peter C. Jørgensen">
+		<meta name="twitter:description" content="The personal website of Peter C. Jørgensen. It includes his portfolio, curriculum vitae, and blog.">
+
+		<!-- JSON-LD Person -->
+		<script type="application/ld+json" defer>
+		{
+		  "@context": "http://schema.org/",
+		  "@type": "Person",
+		  "name": "Peter C. Jørgensen",
+		  "jobTitle": "Multimedia Designer",
+		  "address": {
+		    "@type": "PostalAddress",
+		    "addressLocality": "Odense",
+		    "addressRegion": "Funen"
+		  }
+		}
+		</script>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="/css/skeleton.css">
+		<link rel="stylesheet" type="text/css" href="/css/pcj.min.css">
+
+		<!-- Font -->
+		<link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Raleway:700,600,400,300'>
+
+		<!-- Modernizr -->
+        <script src="/js/modernizr-2.8.3.min.js" defer></script>
+
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TKNTXKZ');</script>
+		<!-- End Google Tag Manager -->
+	</head>
+	<!-- neck -->
+	<body class="home u-full-width">
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKNTXKZ"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
+		<!-- Header -->
+		<header class="container header text-center">
+			<!-- Title -->
+			<h1 class="title">Peter Christian Jørgensen</h1>
+			<hr>
+			<!-- Navigation -->
+			<nav>
+				<div class="row">
+					<section class="three columns"><a href="/files/CV-Peter-Christian-Jørgensen.pdf">C.V.</a></section>
+					<section class="three columns">&nbsp</section>
+					<!-- <section class="three columns"><a href="#">Blog</a></section> -->
+					<!-- Social -->
+					<section class="three columns">
+						<a href="mailto:hello@peterchrjoergensen.dk?subject=Hello" target="_top" class="icon">
+							<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16"><g><path data-color="color-2" fill="#0f0f0f" d="M15,1H1C0.4,1,0,1.4,0,2v1.4l8,4.5l8-4.4V2C16,1.4,15.6,1,15,1z"></path> <path fill="#0f0f0f" d="M7.5,9.9L0,5.7V14c0,0.6,0.4,1,1,1h14c0.6,0,1-0.4,1-1V5.7L8.5,9.9C8.22,10.04,7.78,10.04,7.5,9.9z"></path></g></svg>
+						</a>
+						<a href="https://www.linkedin.com/in/peterchrjoergensen" target="_blank" rel="noopener" class="icon">
+							<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16"><g><path fill="#0f0f0f" d="M15.3,0H0.7C0.3,0,0,0.3,0,0.7v14.7C0,15.7,0.3,16,0.7,16h14.7c0.4,0,0.7-0.3,0.7-0.7V0.7 C16,0.3,15.7,0,15.3,0z M4.7,13.6H2.4V6h2.4V13.6z M3.6,5C2.8,5,2.2,4.3,2.2,3.6c0-0.8,0.6-1.4,1.4-1.4c0.8,0,1.4,0.6,1.4,1.4 C4.9,4.3,4.3,5,3.6,5z M13.6,13.6h-2.4V9.9c0-0.9,0-2-1.2-2c-1.2,0-1.4,1-1.4,2v3.8H6.2V6h2.3v1h0c0.3-0.6,1.1-1.2,2.2-1.2 c2.4,0,2.8,1.6,2.8,3.6V13.6z"></path></g></svg>
+						</a>
+						<a href="https://www.twitter.com/tehwave" target="_blank" rel="noopener" class="icon">
+							<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16"><g><path fill="#0f0f0f" d="M16,3c-0.6,0.3-1.2,0.4-1.9,0.5c0.7-0.4,1.2-1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8c-0.6-0.6-1.5-1-2.4-1 C9.3,1.5,7.8,3,7.8,4.8c0,0.3,0,0.5,0.1,0.7C5.2,5.4,2.7,4.1,1.1,2.1c-0.3,0.5-0.4,1-0.4,1.7c0,1.1,0.6,2.1,1.5,2.7 c-0.5,0-1-0.2-1.5-0.4c0,0,0,0,0,0c0,1.6,1.1,2.9,2.6,3.2C3,9.4,2.7,9.4,2.4,9.4c-0.2,0-0.4,0-0.6-0.1c0.4,1.3,1.6,2.3,3.1,2.3 c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.5,0.9,3.2,1.5,5,1.5c6,0,9.3-5,9.3-9.3c0-0.1,0-0.3,0-0.4C15,4.3,15.6,3.7,16,3z"></path></g></svg>
+						</a>
+					</section>
+				</div>
+			</nav>
+		</header>
+
+		<!-- Main -->
+		<main class="u-full-width">
+			<!-- Introduction -->
+			<section class="parallax intro" data-parallax="scroll" data-speed="0.6" data-image-src="/img/home.jpg" data-natural-width="1920" data-natural-height="1080" data-z-index="0">
+				<div class="container">
+					<h2>Hello,</h2>
+					<p>My name is <b>Peter.</b> It's nice to meet you.</p>
+					<p>I am a student of <b>Multimedia Design</b> in <b>Denmark</b> with a specialty in <b>Film & Animation</b>, and I like to work with <a href="https://twitter.com/search?f=tweets&vertical=default&q=%23webdev%20AND%20from%3A%40tehwave" target="_blank" rel="noopener" class="twitter-search">#webdev</a> & <a href="https://twitter.com/search?f=tweets&vertical=default&q=%23gamedev%20AND%20from%3A%40tehwave" target="_blank" rel="noopener" class="twitter-search">#gamedev</a> as a hobby.</p>
+				</div>
+			</section>
+
+			<!-- Projects -->
+			<section class="container projects text-center">
+				<h3>I'm proud of...</h3>
+				<hr>
+				<div class="row">
+					<div class="one-third column project">
+						<h2>FIRKANT</h2>
+						<p class="lead">A fast-paced, procedural platformer</p>
+						<div><a class="button" href="/FIRKANT" target="_blank">Check it out</a></div>
+					</div>
+					<div class="one-third column project">
+						<h2>B2B Kolding</h2>
+						<p class="lead">A website for a trade fair</p>
+						<div><a class="button" href="https://www.b2bkolding.dk" target="_blank" rel="noopener">Take a browse</a></div>
+					</div>
+					<div class="one-third column project">
+						<h2>GM48</h2>
+						<p class="lead">A quarterly GameMaker game jam</p>
+						<div><a class="button" href="http://www.gm48.net/" target="_blank" rel="noopener">Visit the website</a></div>
+					</div>
+				</div>
+			</section>
+		</main>
+
+		<!-- Footer -->
+		<?php
+			require_once('footer.php');
+		?>
+
+		<!-- JS -->
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js" defer></script>
+		<script src="/js/parallax.js" ></script>
+	</body>
+</html>
