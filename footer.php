ca5434d1a6dd6a3ac359bc41adfb26927474554e
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-09 15:19:27 +0200

    Add - title to links for accessibility

--- footer.php
+++ footer.php
@@ -8,13 +8,13 @@
 					<h2>Get in touch</h2>
 				</div>
 				<div class="one-half column">
-					<a href="mailto:hello@peterchrjoergensen.dk?subject=Hello" target="_top">
+					<a href="mailto:hello@peterchrjoergensen.dk?subject=Hello" target="_top" title="Send me an email">
 						<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="48px" height="48px" viewBox="0 0 48 48"><g> <path data-color="color-2" fill="#ffffff" d="M45,5H3C1.895,5,1,5.895,1,7v6c0,0.366,0.2,0.703,0.521,0.878l22,12 C23.67,25.959,23.835,26,24,26s0.33-0.041,0.479-0.122l22-12C46.8,13.703,47,13.366,47,13V7C47,5.895,46.105,5,45,5z"></path> <path fill="#ffffff" d="M25.437,27.634C24.994,27.874,24.498,28,24,28s-0.994-0.126-1.433-0.364L1.023,15.885 C1.011,15.892,1.013,15.891,1,15.898V41c0,1.105,0.895,2,2,2h42c1.105,0,2-0.895,2-2V15.898c-0.013-0.007-0.011-0.006-0.023-0.014 L25.437,27.634z"></path> </g></svg>
 					</a>
-					<a href="https://www.linkedin.com/in/peterchrjoergensen" target="_blank" rel="noopener">
+					<a href="https://www.linkedin.com/in/peterchrjoergensen" target="_blank" rel="noopener" title="Find me on LinkedIn">
 						<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="48px" height="48px" viewBox="0 0 48 48"><g> <path fill="#ffffff" d="M42,0H6C2.7,0,0,2.7,0,6v36c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V6C48,2.7,45.3,0,42,0z M17.4,36h-5V20h5V36z M14.9,17.8c-1.6,0-2.9-1.3-2.9-2.9c0-1.6,1.3-2.9,2.9-2.9c1.6,0,2.9,1.3,2.9,2.9C17.8,16.5,16.5,17.8,14.9,17.8z M36,36h-5v-7.8 c0-1.9,0-4.2-2.6-4.2c-2.6,0-3,2-3,4.1V36h-5V20h4.8v2.2h0.1c0.7-1.3,2.3-2.6,4.7-2.6c5,0,6,3.3,6,7.6V36z"></path> </g></svg>
 					</a>
-					<a href="https://www.twitter.com/tehwave" target="_blank" rel="noopener">
+					<a href="https://www.twitter.com/tehwave" target="_blank" rel="noopener" title="Find me on Twitter">
 						<svg class="nc-icon glyph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="48px" height="48px" viewBox="0 0 48 48"><g> <path fill="#ffffff" d="M42,0H6C2.7,0,0,2.7,0,6v36c0,3.3,2.7,6,6,6h36c3.3,0,6-2.7,6-6V6C48,2.7,45.3,0,42,0z M33.5,19.1 c0,0.2,0,0.4,0,0.6c0,6.5-5,14-14,14c-2.8,0-5.4-0.8-7.5-2.2c0.4,0,0.8,0.1,1.2,0.1c2.3,0,4.4-0.8,6.1-2.1c-2.2,0-4-1.5-4.6-3.4 c0.3,0.1,0.6,0.1,0.9,0.1c0.4,0,0.9-0.1,1.3-0.2c-2.3-0.5-3.9-2.4-3.9-4.8c0,0,0,0,0-0.1c0.7,0.4,1.4,0.6,2.2,0.6 c-1.3-0.9-2.2-2.4-2.2-4.1c0-0.9,0.2-1.7,0.7-2.5c2.4,3,6.1,4.9,10.1,5.1c-0.1-0.4-0.1-0.7-0.1-1.1c0-2.7,2.2-4.9,4.9-4.9 c1.4,0,2.7,0.6,3.6,1.6c1.1-0.2,2.2-0.6,3.1-1.2c-0.4,1.1-1.1,2.1-2.2,2.7c1-0.1,1.9-0.4,2.8-0.8C35.3,17.5,34.5,18.4,33.5,19.1z"></path> </g></svg>
 					</a>
 				</div>
