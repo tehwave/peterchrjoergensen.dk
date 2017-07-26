276741de288ca844c81eff7283a4e515d3e33c89
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-07-26 11:48:12 +0200

    New - index

---
+++ CV/index.php
@@ -0,0 +1,271 @@
+<?php ?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CV – Peter C. Jørgensen</title>
+
+		<!--
+			  ____      _               ____         _
+			 |  _ \ ___| |_ ___ _ __   / ___|       | | ____  _ __ __ _  ___ _ __  ___  ___ _ __
+			 | |_) / _ \ __/ _ \ '__| | |        _  | |/ _//\| '__/ _` |/ _ \ '_ \/ __|/ _ \ '_ \
+			 |  __/  __/ ||  __/ |    | |___ _  | |_| | (//) | | | (_| |  __/ | | \__ \  __/ | | |
+			 |_|   \___|\__\___|_|     \____(_)  \___/ \//__/|_|  \__, |\___|_| |_|___/\___|_| |_|
+																  |___/
+										Designed and developed by
+							Peter C. Jørgensen | peterchrjoergensen.dk | @tehwave
+		 -->
+
+		<!-- SEO -->
+		<meta name="robots" content="index, follow">
+		<meta name="title" content="CV">
+		<meta name="author" content="Peter C. Jørgensen">
+		<meta name="description" content="Peter C. Jørgensen's CV">
+
+		<!-- Favicon -->
+		<link rel="manifest" href="/manifest.json">
+		<meta name="theme-color" content="#0fa0ce">
+		<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="apple-touch-icon" sizes="144x144" href="/img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16">
+
+		<!-- Open Graph -->
+		<meta property="og:type" content="profile">
+		<meta property="profile:first_name" content="Peter">
+		<meta property="profile:last_name" content="Jørgensen">
+		<meta property="og:title" content="CV">
+		<meta property="og:url" content="https://peterchrjoergensen.dk">
+		<meta property="og:image" content="https://peterchrjoergensen.dk/img/peter.jpg">
+		<meta property="og:description" content="Peter C. Jørgensen's CV">
+
+		<!-- Twitter -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:site" content="@tehwave">
+		<meta name="twitter:title" content="CV">
+		<meta name="twitter:description" content="Peter C. Jørgensen's CV">
+
+		<!-- JSON-LD Person -->
+		<script type="application/ld+json" defer>
+		{
+		  "@context": "http://schema.org/",
+		  "@type": "Person",
+		  "name": "Peter C. Jørgensen",
+		  "jobTitle": "Multimedia Designer",
+		  "address": {
+			"@type": "PostalAddress",
+			"addressLocality": "Kolding",
+			"addressRegion": "Jutland"
+		  }
+		}
+		</script>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="/css/normalize.min.css">
+		<link rel="stylesheet" type="text/css" href="/css/skeleton.min.css">
+		<link rel="stylesheet" type="text/css" href="/css/pcj.min.css">
+
+		<!-- Font -->
+		<link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Raleway:700,600,400,300'>
+
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TKNTXKZ');</script>
+		<!-- End Google Tag Manager -->
+
+		<!-- JavaScript Enabled Check -->
+		<script type="text/javascript">
+			document.documentElement.className = document.documentElement.className.replace("no-js","js");
+		</script>
+	</head>
+	<!-- neck -->
+	<body class="portfolio u-full-width">
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKNTXKZ"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
+		<!-- Header -->
+		<?php
+			require_once('../header.php');
+		?>
+
+		<!-- Main -->
+		<main class="u-full-width">
+			<!-- Introduction -->
+			<section class="parallax intro" data-parallax="scroll" data-speed="0.6" data-image-src="img/home.jpg" data-natural-width="1920" data-natural-height="1080" data-z-index="0">
+				<div class="container">
+					<section class="three columns">
+						<img src="img/peter.png" class="u-max-full-width">
+					</section>
+					<section class="four columns">
+						<h2>Peter Christian Jørgensen</h2>
+					</section>
+				</div>
+			</section>
+
+			<!-- Download -->
+			<div class="container">
+				<section class="twelve columns">
+					<a href="files/CV-Peter-Christian-Jørgensen-English.pdf" target="_blank" rel="noopener noreferrer" class="button">
+						Download C.V. Summary (English)
+					</a>
+					<a href="files/CV-Peter-Christian-Jørgensen-Dansk.pdf" target="_blank" rel="noopener noreferrer" class="button">
+						Download C.V. Summary (Dansk)
+					</a>
+				</section>
+			</div>
+
+			<!-- Education -->
+			<div class="container" style="margin-top: 20px;">
+				<section class="twelve columns">
+					<h2>Education</h2>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2015 - 2017
+				</section>
+				<section class="eight columns">
+					<strong>Multimedia Designer</strong>
+					<p>Lillebaelt Academy</p>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2010 - 2013
+				</section>
+				<section class="eight columns">
+					<strong>Higher Technical Examination</strong>
+					<p>Hansenberg</p>
+				</section>
+			</div>
+
+			<!-- Experience -->
+			<div class="container" style="margin-top: 20px;">
+				<section class="twelve columns">
+					<h2>Experience</h2>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2017
+				</section>
+				<section class="eight columns">
+					<strong>Student Assistant</strong>
+					<div>Grundfos</div>
+					<small><p>A new web application developed in VueJS, and designed to guide visitors at Grundfos facilities through a safety course and -quiz.</p></small>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2017
+				</section>
+				<section class="eight columns">
+					<strong>Intern</strong>
+					<div>B2B Kolding</div>
+					<small><p>Setup a website with Wordpress and a custom designed & developed theme, including a system to handle registering of booths and guests.</p></small>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2013 - 2016
+				</section>
+				<section class="eight columns">
+					<strong>Community Manager</strong>
+					<div>B2B Kolding</div>
+					<small><p>Maintain the day-to-day by removing any unwanted posts and comments. The forum consisted of over 15,000 members and received over 200,000 visitors.</p></small>
+				</section>
+			</div>
+
+			<!-- Projects -->
+			<div class="container" style="margin-top: 20px;">
+				<section class="twelve columns">
+					<h2>Projects</h2>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2016 -
+				</section>
+				<section class="eight columns">
+					<strong>FIRKANT</strong>
+					<div>Mobile game for Android & iOS</div>
+					<small><p>FIRKANT is a fast-paced, procuderal platformer. It is being developed in GameMaker: Studio, and is planned for release in late 2017.</p></small>
+				</section>
+			</div>
+			<div class="container">
+				<section class="two columns">
+					2013 -
+				</section>
+				<section class="eight columns">
+					<strong>Game Development Competition</strong>
+					<div>https://gm48.net</div>
+					<small><p>A quarterly, non-profit games development competition, better known as a game jam, in which the participants use the integrated development environment, GameMaker, to develop games from scratch in just 48 hours.</p></small>
+				</section>
+			</div>
+			<div class="container">
+				<section class="offset-by-two eight columns">
+					<strong>See more on my <a href="/portfolio">portfolio</a>.</strong>
+				</section>
+			</div>
+		</main>
+
+		<!-- Footer -->
+		<?php
+			require_once('../footer.php');
+		?>
+
+		<!-- JS -->
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js" defer></script>
+		<script src="/js/parallax.min.js" defer></script>
+
+		<!-- LazyLoad -->
+		<script src="/js/lazysizes.min.js" async></script>
+
+		<!-- MixitUp -->
+		<script src="/js/mixitup.min.js"></script>
+		<script type="text/javascript">
+			var mixer = mixitup('#mixitup', {
+			    selectors: {
+			        target: '.project'
+			    },
+			    animation: {
+			        duration: 400,
+			        nudge: true,
+			        effects: "fade translateZ(-100px)"
+			    },
+			    callbacks: {
+					onMixEnd: function() {
+						jQuery(window).trigger('resize').trigger('scroll');
+					}
+			    }
+			});
+
+			// Detect if positioned at top
+			function posTop() {
+	            return typeof window.pageYOffset != 'undefined' ? window.pageYOffset: document.documentElement.scrollTop? document.documentElement.scrollTop: document.body.scrollTop? document.body.scrollTop:0;
+	        }
+
+	        var page = $("html, body");
+
+			$("button").click(function() {
+				page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+					page.stop();
+				});
+
+				if (!posTop()) {
+					page.animate({ scrollTop: $("#mixitup").position().top - 120 }, 1500, function(){
+						page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+					});
+				};
+			});
+		</script>
+	</body>
+</html>
