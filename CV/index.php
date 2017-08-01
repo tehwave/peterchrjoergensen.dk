918e8e6d5e0198072ca8affe9f784199616ace8f
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-01 07:39:51 +0200

    Remove - unused scripts

--- CV/index.php
+++ CV/index.php
@@ -222,50 +222,7 @@
 
 		<!-- JS -->
 		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js" defer></script>
 		<script src="/js/parallax.min.js" defer></script>
-
-		<!-- LazyLoad -->
-		<script src="/js/lazysizes.min.js" async></script>
-
-		<!-- MixitUp -->
-		<script src="/js/mixitup.min.js"></script>
-		<script type="text/javascript">
-			var mixer = mixitup('#mixitup', {
-			    selectors: {
-			        target: '.project'
-			    },
-			    animation: {
-			        duration: 400,
-			        nudge: true,
-			        effects: "fade translateZ(-100px)"
-			    },
-			    callbacks: {
-					onMixEnd: function() {
-						jQuery(window).trigger('resize').trigger('scroll');
-					}
-			    }
-			});
-
-			// Detect if positioned at top
-			function posTop() {
-	            return typeof window.pageYOffset != 'undefined' ? window.pageYOffset: document.documentElement.scrollTop? document.documentElement.scrollTop: document.body.scrollTop? document.body.scrollTop:0;
-	        }
-
-	        var page = $("html, body");
-
-			$("button").click(function() {
-				page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
-					page.stop();
-				});
-
-				if (!posTop()) {
-					page.animate({ scrollTop: $("#mixitup").position().top - 120 }, 1500, function(){
-						page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
-					});
-				};
-			});
-		</script>
 	</body>
 </html>
