1419f724c1f68a70b58215c5289fa5dbd85d3ffb
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-02 09:44:58 +0200

    Change - move projects into array, and use php to parse it

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,4 +1,120 @@
-<?php ?>
+<?php
+
+$projects = array(
+	"FIRKANT" => array(
+    	"filters" => array("game", "solo"),
+		"image" => "firkant",
+		"image_format" => "png",
+		"title" => "FIRKANT",
+		"tags" => array("Game", "GameMaker: Studio", "Solo"),
+		"lead" => "FIRKANT is a a fast-paced, procedural platformer inspired by Cloudberry Kingdom and Super Meat Boy.",
+		"paragraph" => "Made in GameMaker: Studio, it's planned for release on mobile platforms in late 2017. For more information, please visit the website.",
+		"button_text" => "Check it out",
+		"button_url" => "/FIRKANT"
+	),
+	"B2B Kolding" => array(
+    	"filters" => array("website", "company"),
+		"image" => "b2bkolding",
+		"image_format" => "png",
+		"title" => "B2B Kolding",
+		"tags" => array("Website", "Wordpress", "Company"),
+		"lead" => "B2B Kolding aims to strengthen the partnership between local businesses in Kolding, Denmark.",
+		"paragraph" => "Using Wordpress as CMS, I developed a custom-built theme for the website, and optimized it with cache, SEO and analytics plugins.",
+		"button_text" => "Check it out",
+		"button_url" => "https://www.b2bkolding.dk"
+	),
+	"GM48" => array(
+    	"filters" => array("website", "solo"),
+		"image" => "gm48",
+		"image_format" => "png",
+		"title" => "GM48",
+		"tags" => array("Website", "PHP", "Solo"),
+		"lead" => "The gm(48) is a quarterly 48 hours GameMaker game jam from the community of /r/GameMaker.",
+		"paragraph" => "I wrote the website in PHP, HTML, SASS and JS using the Laravel, Bootstrap and VueJS frameworks.",
+		"button_text" => "Check it out",
+		"button_url" => "https://gm48.net"
+	),
+	"Grundfos Safety" => array(
+    	"filters" => array("website", "company"),
+		"image" => "grundfos",
+		"image_format" => "png",
+		"title" => "Grundfos Safety",
+		"tags" => array("Website", "JavaScript", "Company"),
+		"lead" => "A safety instructions and quiz web application for visitors to the Grundfos facilities.",
+		"paragraph" => "The web application was written in VueJS, and has editable files, translations and support for browsers from IE9 and up.",
+		"button_text" => "Check it out",
+		"button_url" => "https://tehwave.github.io/grundfos-quiz/"
+	),
+	"The Author" => array(
+    	"filters" => array("video", "team"),
+		"image" => "author",
+		"image_format" => "jpg",
+		"title" => "The Author",
+		"tags" => array("Video", "Premiere Pro", "Team"),
+		"lead" => "An author with writer's block goes to an abandoned house for inspiration, but find things getting too real.",
+		"paragraph" => "I starred in the short film as the main character. It was colorgraded and edited in Adobe Premiere Pro by me as well.",
+		"button_text" => "Watch it",
+		"button_url" => "https://www.youtube.com/watch?v=S0bQYYnhtGk"
+	),
+	"Western World" => array(
+    	"filters" => array("application", "team"),
+		"image" => "western",
+		"image_format" => "jpg",
+		"title" => "Western World",
+		"tags" => array("Application", "Unity3D", "Team"),
+		"lead" => "A small, atmospheric town, that was inspired by the good, old Western films, to explore around in.",
+		"paragraph" => "Made in Unity3D as a school project, it was meant to teach us the asset pipeline between Maya and Unity.",
+		"button_text" => "Watch the trailer",
+		"button_url" => "https://www.youtube.com/watch?v=0SeTUIsS5sQ"
+	),
+	"Showreel - 2015" => array(
+    	"filters" => array("video", "solo"),
+		"image" => "showreel",
+		"image_format" => "jpg",
+		"title" => "Showreel - 2015",
+		"tags" => array("Video", "Premiere Pro", "Solo"),
+		"lead" => "A showreel for various school projects that I completed during my first two semesters in 2015.",
+		"paragraph" => "Edited in Premiere Pro, it was important for me that the music matched the editing, so that it was exciting to watch.",
+		"button_text" => "Watch it",
+		"button_url" => "https://www.youtube.com/watch?v=cYLeWeJryOQ"
+	),
+	"My Website" => array(
+    	"filters" => array("website", "solo"),
+		"image" => "website",
+		"image_format" => "png",
+		"title" => "My Website",
+		"tags" => array("Website", "HTML / CSS / JS", "Solo"),
+		"lead" => "My personal website complete with landing page, portfolio, curriculum vitae and blog.",
+		"paragraph" => "I wrote the website to be static and fast using plain HTML, CSS and JS. I used MixItUp 3 for display & filtering of projects.",
+		"button_text" => "Browse code",
+		"button_url" => "https://github.com/tehwave/peterchrjoergensen.dk"
+	),
+	"Mord Ombord" => array(
+    	"filters" => array("game", "team"),
+		"image" => "mordombord",
+		"image_format" => "png",
+		"title" => "Mord Ombord",
+		"tags" => array("Game", "Unity3D", "Team"),
+		"lead" => "Solve mysteries and puzzles using learning material in Virtual Reality as part of your school education.",
+		"paragraph" => "Developed in Unity3D for use with Google Cardboard, I had to heavily optimize the game to run on old devices.",
+		"button_text" => "Watch trailer",
+		"button_url" => "https://www.youtube.com/watch?v=0U42shiUG2w"
+	),
+	"Odense Golfklub" => array(
+    	"filters" => array("application", "team"),
+		"image" => "odensegolfklub",
+		"image_format" => "png",
+		"title" => "Odense Golfklub",
+		"tags" => array("Application", "Unity3D", "Team"),
+		"lead" => "A mobile application for the members of Odense Golfklub with 360° imagery and 3D models of golf courses.",
+		"paragraph" => "Developed in Unity3D for its ease with 3D, I had to write custom functionality to handle the display of 360° imagery.",
+		"button_text" => "Download APK",
+		"button_url" => "files/OdenseGolfklub.apk"
+	),
+);
+
+
+?>
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 	<head>
@@ -119,182 +235,39 @@
 				</ul>
 				<div class="container" id="mixitup">
 					<hr>
-					<article class="project game solo">
-						<noscript><img class="u-max-full-width image" src="img/firkant.png" width="300" height="300" alt="FIRKANT"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/firkant-lowquality.png" data-src="img/firkant.png" width="300" height="300" alt="FIRKANT">
-						<h2 class="title">FIRKANT</h2>
-						<hr>
-						<ul class="tags">
-							<li>Game</li>
-							<li>GameMaker: Studio</li>
-							<li>Solo</li>
-						</ul>
-						<p class="lead">FIRKANT is a a fast-paced, procedural platformer inspired by Cloudberry Kingdom and Super Meat Boy.</p>
-						<p>Made in GameMaker: Studio, it's planned for release on mobile platforms in late 2017. For more information, please visit the website.</p>
-						<div><a class="button button-primary" href="/FIRKANT" target="_blank" rel="noopener">Check it out</a></div>
-					</article>
-					<article class="project website company">
-						<noscript><img class="u-max-full-width image" src="img/b2bkolding.png" width="300" height="300" alt="B2B Kolding"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/b2bkolding-lowquality.png" data-src="img/b2bkolding.png" width="300" height="300" alt="B2B Kolding">
-						<h2 class="title">B2B Kolding</h2>
-						<hr>
-						<ul class="tags">
-							<li>Website</li>
-							<li>Wordpress</li>
-							<li>Company</li>
-						</ul>
-						<p class="lead">B2B Kolding aims to strengthen the partnership between local businesses in Kolding, Denmark.</p>
-						<p>Using Wordpress as CMS, I developed a custom-built theme for the website, and optimized it with cache, SEO and analytics plugins.</p>
-						<div><a class="button button-primary" href="https://www.b2bkolding.dk" target="_blank" rel="noopener">Check it out</a></div>
-					</article>
-					<article class="project website solo">
-						<noscript><img class="u-max-full-width image" src="img/gm48.png" width="300" height="300" alt="gm(48)"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/gm48-lowquality.png" data-src="img/gm48.png" width="300" height="300" alt="gm(48)">
-						<h2 class="title">GM48</h2>
-						<hr>
-						<ul class="tags">
-							<li>Website</li>
-							<li>Laravel</li>
-							<li>Solo</li>
-						</ul>
-						<p class="lead">The gm(48) is a quarterly 48 hours GameMaker game jam from the community of /r/GameMaker.</p>
-						<p>I wrote the website in PHP, HTML, SASS and JS using the Laravel, Bootstrap and VueJS frameworks.</p>
-						<div><a class="button button-primary" href="https://gm48.net" target="_blank" rel="noopener">Check it out</a></div>
-					</article>
-					<article class="project website company">
-						<noscript><img class="u-max-full-width image" src="img/grundfos.png" width="300" height="300" alt="Grundfos"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/grundfos-lowquality.png" data-src="img/grundfos.png" width="300" height="300" alt="Grundfos">
-						<h2 class="title">Grundfos Safety</h2>
-						<hr>
-						<ul class="tags">
-							<li>Website</li>
-							<li>VueJS</li>
-							<li>Company</li>
-						</ul>
-						<p class="lead">A safety instructions and quiz web application for visitors to the Grundfos facilities.</p>
-						<p>The web application was written in VueJS, and has editable files, translation and support for browsers IE9+.</p>
-						<div><a class="button button-primary" href="https://tehwave.github.io/grundfos-quiz/" target="_blank" rel="noopener">Check it out</a></div>
-					</article>
-					<article class="project video team">
-						<noscript><img class="u-max-full-width image" src="img/author.jpg" width="300" height="300" alt="The Author"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/author-lowquality.jpg" data-src="img/author.jpg" width="300" height="300" alt="The Author">
-						<h2 class="title">The Author</h2>
-						<hr>
-						<ul class="tags">
-							<li>Video</li>
-							<li>Premiere Pro</li>
-							<li>Team</li>
-						</ul>
-						<p class="lead">
-							An author with writer's block goes to an abandoned house for inspiration, but find things getting too real.
-						</p>
-						<p>
-							I starred in the short film as the main character. It was colorgraded and edited in Adobe Premiere Pro by me as well.
-						</p>
-						<div>
-							<a class="button button-primary" href="https://www.youtube.com/watch?v=S0bQYYnhtGk" target="_blank" rel="noopener">Watch it</a>
-						</div>
-					</article>
-					<article class="project game team">
-						<noscript><img class="u-max-full-width image" src="img/western.jpg" width="300" height="300" alt="Western World"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/western-lowquality.jpg" data-src="img/western.jpg" width="300" height="300" alt="Western World">
-						<h2 class="title">Western World</h2>
-						<hr>
-						<ul class="tags">
-							<li>Game</li>
-							<li>Unity3D</li>
-							<li>Team</li>
-						</ul>
-						<p class="lead">
-							A small, atmospheric town, that was inspired by the good, old Western films, to explore around in.
-						</p>
-						<p>
-							Made in Unity3D as a school project, it was meant to teach us the asset pipeline between Maya and Unity.
-						</p>
-						<div>
-							<a class="button button-primary" href="https://www.youtube.com/watch?v=0SeTUIsS5sQ" target="_blank" rel="noopener">Watch trailer</a>
-						</div>
-					</article>
-					<article class="project video solo">
-						<noscript><img class="u-max-full-width image" src="img/showreel.jpg" width="300" height="300" alt="Showreel"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/showreel-lowquality.jpg" data-src="img/showreel.jpg" width="300" height="300" alt="Showreel">
-						<h2 class="title">Showreel - 2015</h2>
-						<hr>
-						<ul class="tags">
-							<li>Video</li>
-							<li>Premiere Pro</li>
-							<li>Solo</li>
-						</ul>
-						<p class="lead">
-							A showreel for various school projects that I completed during my first two semesters in 2015.
-						</p>
-						<p>
-							Edited in Premiere Pro, it was important for me that the music matched the editing, so that it was exciting to watch.
-						</p>
-						<div>
-							<a class="button button-primary" href="https://www.youtube.com/watch?v=cYLeWeJryOQ" target="_blank" rel="noopener">Watch it</a>
-						</div>
-					</article>
-					<article class="project website solo">
-						<noscript><img class="u-max-full-width image" src="img/website.png" width="300" height="300" alt="Website"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/website-lowquality.png" data-src="img/website.png" width="300" height="300" alt="Website">
-						<h2 class="title">My Website</h2>
-						<hr>
-						<ul class="tags">
-							<li>Website</li>
-							<li>HTML / CSS / JS</li>
-							<li>Solo</li>
-						</ul>
-						<p class="lead">
-							My personal website complete with landing page, portfolio, curriculum vitae and blog.
-						</p>
-						<p>
-							I wrote the website to be static and fast using plain HTML, CSS and JS. I used MixItUp 3 for projects display & filtering.
-						</p>
-						<div>
-							<a class="button button-primary" href="https://github.com/tehwave/peterchrjoergensen.dk" target="_blank" rel="noopener">Browse code</a>
-						</div>
-					</article>
-					<article class="project game team">
-						<noscript><img class="u-max-full-width image" src="img/mordombord.png" width="300" height="300" alt="Mord Ombord"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/mordombord-lowquality.png" data-src="img/mordombord.png" width="300" height="300" alt="Mord Ombord">
-						<h2 class="title">Mord Ombord</h2>
-						<hr>
-						<ul class="tags">
-							<li>Game</li>
-							<li>Unity3D</li>
-							<li>Team</li>
-						</ul>
-						<p class="lead">
-							Solve mysteries and puzzles in Virtual Reality as part of your base school education
-						</p>
-						<p>
-							Developed in Unity3D for use with Google Cardboard, I had to heavily optimize the game to run on old devices.
-						</p>
-						<div>
-							<a class="button button-primary" href="https://www.youtube.com/watch?v=0U42shiUG2w" target="_blank" rel="noopener">Watch trailer</a>
-						</div>
-					</article>
-					<article class="project application team">
-						<noscript><img class="u-max-full-width image" src="img/odensegolfklub.png" width="300" height="300" alt="Odense Golfklub"></noscript>
-						<img class="u-max-full-width image lazyload lqip" src="img/odensegolfklub-lowquality.png" data-src="img/odensegolfklub.png" width="300" height="300" alt="Odense Golfklub">
-						<h2 class="title">Odense Golfklub</h2>
-						<hr>
-						<ul class="tags">
-							<li>Game</li>
-							<li>Unity3D</li>
-							<li>Team</li>
-						</ul>
-						<p class="lead">
-							A mobile application for the members of Odense Golfklub with 360° imagery and 3D models of golf courses.
-						</p>
-						<p>
-							Developed in Unity3D for its ease with 3D, I had to write custom functionality to handle the display of 360° imagery.
-						</p>
-						<div>
-							<a class="button button-primary" href="files/OdenseGolfklub.apk" target="_blank" rel="noopener">Download APK</a>
-						</div>
-					</article>
+					<?php foreach ($projects as $project): ?>
+						<article class="project <?php echo implode(" ", $project['filters']) ?>">
+							<noscript>
+								<img class="u-max-full-width image"
+								src="img/<?php echo $project['image'] .".". $project['image_format'] ?>"
+								width="300" height="300" alt="<?php echo $project['title'] ?>"/>
+							</noscript>
+							<img class="u-max-full-width image lazyload lqip"
+								src="img/<?php echo $project['image'] ?>-lowquality.<?php echo $project['image_format'] ?>"
+								data-src="img/<?php echo $project['image'] .".". $project['image_format'] ?>"
+								width="300" height="300" alt="<?php echo $project['title'] ?>"/>
+							<h2 class="title">
+								<?php echo $project['title'] ?>
+							</h2>
+							<hr>
+							<ul class="tags">
+								<?php
+									foreach ($project['tags'] as $tag) {
+										echo "<li>". $tag ."</li>\r\n";
+									}
+								?>
+							</ul>
+							<p class="lead">
+								<?php echo $project['lead'] ?>
+							</p>
+							<p>
+								<?php echo $project['paragraph'] ?>
+							</p>
+							<a class="button button-primary" href="<?php echo $project['button_url'] ?>" target="_blank" rel="noopener">
+								<?php echo $project['button_text'] ?>
+							</a>
+						</article>
+					<?php endforeach; ?>
 				</div>
 			</section>
 		</main>
