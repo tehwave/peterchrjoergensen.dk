3efc1943637afb79843b3b69754a0c0504ace37b
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-08 18:07:07 +0200

    Update - uncomment scripts
    internet went down and page was slow due to scripts not being loading,
    so had to uncomment.
    
    TODO: Get local files?

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -80,11 +80,11 @@
         <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Raleway:700,600,400,300'>
 
         <!-- Google Tag Manager -->
-<!--        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+       <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TKNTXKZ');</script> -->
+        })(window,document,'script','dataLayer','GTM-TKNTXKZ');</script>
         <!-- End Google Tag Manager -->
     </head>
     <!-- neck -->
@@ -160,9 +160,9 @@
         </footer>
 
         <!-- JS -->
-<!--        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous" defer></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js" defer></script>
-        <script src="/resources/js/parallax.min.js" defer></script> -->
+        <script src="/resources/js/parallax.min.js" defer></script>
         <?php if (isset($scripts)): foreach ($scripts as $script): ?>
             <script type="text/javascript" src="<?php echo $script ?>"></script>
         <?php endforeach; endif; ?>
