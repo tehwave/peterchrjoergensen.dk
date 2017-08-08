b409b4ce08fa05db8961786588544ec6ee1d3b52
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-08 19:20:07 +0200

    Add - todo comment

--- config/app.php
+++ config/app.php
@@ -13,6 +13,7 @@ Flight::set('flight.views.path', 'resources/views');
 *
 * @param  string $view Name of view to cache
 */
+// TODO: Move this somewhere else more appropriate.
 Flight::map('cache', function($view) {
     header("Cache-Control: public, max-age=31536000");
     return Flight::lastModified(filemtime(sprintf('./%s/%s%s', Flight::get('flight.views.path'), $view, Flight::get('flight.views.extension'))));
