96fce5e2d5b99387555b8f3be0060ecf46b7db9d
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-08 17:46:39 +0200

    Add - images task for minifying

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     rename      = require('gulp-rename'),
+    imagemin    = require('gulp-imagemin'),
     sourcemaps  = require('gulp-sourcemaps');
 
 var sassPath    = 'resources/sass/*.scss',
-    cssPath     = 'resources/css';
+    cssPath     = 'resources/css',
+    srcPath     = 'resources/src',
+    imgPath     = 'resources/img';
 
 gulp.task('styles', function(){
     gulp.src(sassPath)
@@ -17,12 +20,18 @@ gulp.task('styles', function(){
         .pipe(gulp.dest(cssPath));
 });
 
+gulp.task('images', function() {
+    gulp.src(srcPath + '/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgPath))
+});
+
 gulp.task('watch', function() {
     gulp.watch(sassPath, ['styles']);
 });
 
 gulp.task('default', function() {
-    gulp.start('styles');
+    gulp.start('styles', 'images');
 });
 
 
