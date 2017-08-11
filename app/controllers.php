e491c7e79861030c9e56473129b1436c1fd432f3
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-11 16:18:41 +0200

    Remove - home stylesheet

--- app/controllers.php
+++ app/controllers.php
@@ -28,7 +28,6 @@ class Controller
         return Flight::render('layout', [
             'title'         => 'Peter C. Jørgensen',
             'description'   => 'The personal website of Peter C. Jørgensen. It includes his portfolio, curriculum vitae, and blog.',
-            'stylesheets'   => ['/resources/css/home.min.css'],
         ]);
     }
 
