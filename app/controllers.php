908b23130318fdfac70bbe6b4f27400528358fec
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-13 00:02:25 +0200

    Change - rm cv stylesheet, add lazysizes

--- app/controllers.php
+++ app/controllers.php
@@ -84,7 +84,7 @@ class Controller
         return Flight::render('layout', [
             'title'         => 'Peter C. Jørgensen',
             'description'   => 'The personal website of Peter C. Jørgensen. It includes his portfolio, curriculum vitae, and blog.',
-            'stylesheets'   => ['/resources/css/cv.min.css'],
+            'scripts'       => ['/resources/js/lazysizes.min.js'],
         ]);
     }
 
