0960fe4811672b144aef93e4341fe2d16f7a3013
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-08-22 17:53:26 +0200

    Add - jquery api call to blog api

---
+++ resources/js/home.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+    $.get("https://blog.peterchrjoergensen.dk/wp-json/wp/v2/posts?per_page=3", function( data ) {
+        $('#wp-posts-loadinga').remove();
+        $.each(data, function(i, post) {
+            $( "#wp-postsa").append(
+                '<article class="col-4">' +
+                    '<h1 class="h4">' +
+                        '<a href="' + post.link + '">' +
+                            post.title.rendered +
+                        '</a>' +
+                    '</h1>' +
+                    '<p>' +
+                        post.excerpt.rendered +
+                    '</p>' +
+                '</article>'
+            );
+        });
+    });
+});
