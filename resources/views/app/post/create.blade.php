30a9816121560d1dd1288eeb652773ddf4b1170b
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-12-05 19:45:07 +0100

    Change - layout, add simpleMDE to body textarea

--- resources/views/app/post/create.blade.php
+++ resources/views/app/post/create.blade.php
@@ -4,20 +4,24 @@
 @section('footer', false)
 
 @section('app')
-    <div class="container h-100">
+    <div class="container-fluid h-100">
         <div class="row h-100">
             <div class="my-auto col-lg-8 mx-lg-auto">
                 <form method="POST" action="{{ route('post.store') }}">
                     <article class="card my-4">
                         <header class="card-header bg-pcj text-white">
-                            <h1 class="h6 mb-0">New</h1>
+                            <div class="row">
+                                <section class="col-lg-8 mx-lg-auto">
+                                    <h1 class="h6 mb-0">New</h1>
+                                </section>
+                            </div>
                         </header>
                         <section class="card-body">
                                 {{ csrf_field() }}
 
                                 @unless ($errors->isEmpty())
                                     <div class="form-row">
-                                        <div class="form-group col">
+                                        <div class="form-group  col-lg-8 mx-lg-auto">
                                             <div class="alert alert-danger" role="alert">
                                                 <ul class="list-unstyled mb-0">
                                                     @foreach ($errors->all() as $error)
@@ -30,14 +34,14 @@
                                 @endunless
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group  col-lg-8 mx-lg-auto">
                                         <label for="name">Title</label>
                                         <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required>
                                     </div>
                                 </div>
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group  col-lg-8 mx-lg-auto">
                                         <label for="excerpt">Excerpt</label>
                                         <textarea id="excerpt" type="text" class="form-control" name="excerpt" value="{{ old('excerpt') }}"></textarea>
                                         <ul class="list-inline form-text text-muted mb-0">
@@ -47,7 +51,7 @@
                                 </div>
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group  col-lg-8 mx-lg-auto">
                                         <label for="body">Body</label>
                                         <textarea id="body" type="text" class="form-control" rows="10" name="body" value="{{ old('body') }}"></textarea>
                                         <ul class="list-inline form-text text-muted mb-0">
@@ -58,7 +62,7 @@
                                 </div>
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group  col-lg-8 mx-lg-auto">
                                         <label for="published_at">Publish</label>
                                         <input id="published_at" type="date" class="form-control" name="published_at" value="{{ old('published_at') }}">
                                     </div>
@@ -66,7 +70,7 @@
                         </section>
                         <footer class="card-footer">
                             <div class="form-row">
-                                <div class="col text-right">
+                                <div class=" col-lg-8 mx-lg-auto text-right">
                                     <button type="submit" class="btn btn-pcj">
                                         Create
                                     </button>
@@ -79,3 +83,24 @@
         </div>
     </div>
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+@endpush
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+    <script type="text/javascript">
+        var simplemde = new SimpleMDE({
+            element: $("#body")[0],
+            insertTexts: {
+                image: ['<img src="', '">'],
+            },
+            autosave: {
+                enabled: true,
+                delay: 1000,
+            },
+            spellChecker: false,
+        });
+    </script>
+@endpush
--- resources/views/app/post/edit.blade.php
+++ resources/views/app/post/edit.blade.php
@@ -4,14 +4,19 @@
 @section('footer', false)
 
 @section('app')
-    <div class="container h-100">
+    <div class="container-fluid h-100">
         <div class="row h-100">
             <div class="my-auto col-lg-8 mx-lg-auto">
                 {{-- Update --}}
                 <form method="POST" action="{{ route('post.update', $post->slug) }}">
                     <article class="card my-4">
                         <header class="card-header bg-pcj text-white">
-                            <h1 class="h6 mb-0">Edit</h1>
+                            <div class="row">
+                                <section class="col-lg-8 mx-lg-auto">
+                                    <h1 class="h6 mb-0">Edit</h1>
+                                    <h2 class="mb-0">{{ $post->title }}</h2>
+                                </section>
+                            </div>
                         </header>
                         <section class="card-body">
                                 {{ csrf_field() }}
@@ -19,7 +24,7 @@
 
                                 @unless ($errors->update->isEmpty())
                                     <div class="form-row">
-                                        <div class="form-group col">
+                                        <div class="form-group col-lg-8 mx-lg-auto">
                                             <div class="alert alert-danger" role="alert">
                                                 <ul class="list-unstyled mb-0">
                                                     @foreach ($errors->update->all() as $error)
@@ -32,17 +37,16 @@
                                 @endunless
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group col-lg-8 mx-lg-auto">
                                         <label for="name">Title</label>
                                         <input id="title" type="text" class="form-control" name="title" value="{{ old('title', $post->title) }}" required>
-
                                     </div>
                                 </div>
 
 
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group col-lg-8 mx-lg-auto">
                                         <label for="excerpt">Excerpt</label>
                                         <textarea id="excerpt" type="text" class="form-control" name="excerpt">{{ old('excerpt', $post->excerpt) }}</textarea>
                                         <ul class="list-inline form-text text-muted mb-0">
@@ -52,7 +56,7 @@
                                 </div>
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group col-lg-8 mx-lg-auto">
                                         <label for="body">Body</label>
                                         <textarea id="body" type="text" class="form-control" rows="10" name="body">{{ old('body', $post->body) }}</textarea>
                                         <ul class="list-inline form-text text-muted mb-0">
@@ -63,7 +67,7 @@
                                 </div>
 
                                 <div class="form-row">
-                                    <div class="form-group col">
+                                    <div class="form-group col-lg-8 mx-lg-auto">
                                         <label for="published_at">Publish</label>
                                         <input id="published_at" type="date" class="form-control" name="published_at" value="{{ old('published_at', optional($post->published_at)->toDateString()) }}">
                                     </div>
@@ -71,7 +75,7 @@
                         </section>
                         <footer class="card-footer">
                             <div class="form-row">
-                                <div class="col text-right">
+                                <div class="col-lg-8 mx-lg-auto text-right">
                                     <button type="submit" class="btn btn-pcj">
                                         Update
                                     </button>
@@ -106,7 +110,7 @@
                                 @endunless
 
                                 <div class="form-row">
-                                    <div class="form-group col mb-0">
+                                    <div class="form-group col-lg-8 mx-lg-auto mb-0">
                                         <div class="form-check">
                                             <label class="form-check-label">
                                                 <input class="form-check-input" type="checkbox" name="delete" value="1"> I want to delete {{ $post->title }}
@@ -117,7 +121,7 @@
                         </section>
                         <footer class="card-footer">
                             <div class="form-row">
-                                <div class="col text-right">
+                                <div class="col-lg-8 mx-lg-auto text-right">
                                     <button type="submit" class="btn btn-danger">
                                         Destroy
                                     </button>
@@ -130,3 +134,25 @@
         </div>
     </div>
 @endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+@endpush
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+    <script type="text/javascript">
+        var simplemde = new SimpleMDE({
+            element: $("#body")[0],
+            insertTexts: {
+                image: ['<img src="', '">'],
+            },
+            autosave: {
+                enabled: true,
+                uniqueId: "post{{ $post->id }}",
+                delay: 1000,
+            },
+            spellChecker: false,
+        });
+    </script>
+@endpush
