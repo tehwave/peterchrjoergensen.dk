5a748f493bbd725667bef1c421a8da808867159a
Peter 🌊 Jørgensen <peterchrjoergensen@gmail.com>
2017-12-19 19:55:12 +0100

    Change - layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,9 +10,8 @@
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Title</th>
                     <th></th>
+                    <th>Title</th>
                     <th>Tags</th>
                     <th class="text-right">Published</th>
                     <th class="text-right">Created</th>
@@ -22,13 +21,12 @@
             <tbody>
                 @foreach ($posts as $post)
                     <tr>
-                        <td class="align-middle">{{ $post->id }}</td>
-                        <td class="align-middle"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></td>
                         <td class="align-middle"><a href="{{ route('post.edit', $post->slug) }}" class="btn btn-pcj btn-sm">Edit</a></td>
+                        <td class="align-middle"><a href="{{ route('post.show', $post->slug) }}">{{ $post->title }}</a></td>
                         <td class="align-middle">@foreach ($post->tags as $tag) <span class="badge badge-pill badge-pcj">{{ $tag->name }}</span> @endforeach</td>
-                        <td class="align-middle text-right">{{ $post->published_at }}</td>
-                        <td class="align-middle text-right">{{ $post->created_at }}</td>
-                        <td class="align-middle text-right">{{ $post->updated_at }}</td>
+                        <td class="align-middle text-right">@if ($post->published_at) {{ $post->published_at->toFormattedDateString() }} @else Draft @endif</td>
+                        <td class="align-middle text-right">{{ $post->created_at->toFormattedDateString() }}</td>
+                        <td class="align-middle text-right">{{ $post->updated_at->toFormattedDateString() }}</td>
                     </tr>
                 @endforeach
             </tbody>
