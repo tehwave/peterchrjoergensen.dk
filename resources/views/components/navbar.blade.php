bb2bf4914483a065b5e99f94c43d27b1eec10522
Peter 🌊 Jørgensen <peterchrjoergensen@gmail.com>
2018-03-18 18:00:36 +0100

    Remove obsolete navbar-dark

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <nav class="navbar navbar-dark navbar-expand-lg navbar-pcj">
+    <nav class="navbar navbar-expand-lg navbar-pcj">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" width="16" height="16" class="align-bottom"><g class="nc-icon-wrapper" fill="#ffffff"><rect fill="#ffffff" width="16" height="3"></rect> <rect data-color="color-2" y="6" fill="#ffffff" width="16" height="3"></rect> <rect y="12" fill="#ffffff" width="16" height="3"></rect></g></svg>
             Browse
