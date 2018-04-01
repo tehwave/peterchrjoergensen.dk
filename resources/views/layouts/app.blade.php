6401cd2694ebbe29e3efef255b9115667138b2a2
Peter 🌊 Jørgensen <peterchrjoergensen@gmail.com>
2018-04-01 14:49:48 +0200

    Add google adsense to header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,15 @@
             })(window,document,'script','dataLayer','GTM-TKNTXKZ');
         </script>
 
+        <!-- Google AdSense -->
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-4880846761046037",
+            enable_page_level_ads: true
+          });
+        </script>
+
         <!-- Fonts -->
 
         <!-- Styles -->
