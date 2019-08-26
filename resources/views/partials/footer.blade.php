f0138b9309a4e4d38f5ff25c5c36827958744f67
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2019-08-26 19:33:47 +0200

    fix footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,17 +7,16 @@
             <div class="col-6 col-md-3">
                 <p class="text-secondary mb-0 font-semibold">Get in touch</p>
                 <ul class="list-unstyled">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Email</a></li>
+                    <li><a href="https://www.linkedin.com/in/peterchrjoergensen">LinkedIn</a></li>
+                    <li><a href="https://www.twitter.com/tehwave">Twitter</a></li>
                 </ul>
             </div>
             <div class="col-6 col-md-3">
                 <p class="text-secondary mb-0 font-semibold">On the internet</p>
                 <ul class="list-unstyled">
-                    <li><a href="#">YouTube</a></li>
-                    <li><a href="#">Patreon</a></li>
-                    <li><a href="#">GitHub</a></li>
+                    <li><a href="https://www.youtube.com/user/tehwave">YouTube</a></li>
+                    <li><a href="https://www.patreon.com/tehwave">Patreon</a></li>
+                    <li><a href="https://www.github.com/tehwave">GitHub</a></li>
                 </ul>
             </div>
         </div>
