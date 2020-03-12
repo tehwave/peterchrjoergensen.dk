138a69da8484ad51ce1f81b40e6dba45398dff1b
Peter Jørgensen <peterchrjoergensen@gmail.com>
2020-03-12 20:24:40 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMix();
+    }
 }
